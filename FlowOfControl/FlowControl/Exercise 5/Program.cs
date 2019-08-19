69d3289176686857a805e791cb4edd43c31bd2f0
Janis Strazdins <scan@inbox.lv>
2019-08-19 19:42:39 +0300

    added Basics&FlowControl&TypesAndVariables

---
+++ FlowOfControl/FlowControl/Exercise 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 7;
+            string output = "";
+
+            switch (i)
+            {
+                case 1:
+                    output = "Pirmdiena";
+                    break;
+                case 2:
+                    output = "Pirmdiena";
+                    break;
+                default:
+                    output = "Nav tadas dienas";
+                    break;
+            }
+            
+
+            Console.WriteLine(output);
+        }
+    }
+}
