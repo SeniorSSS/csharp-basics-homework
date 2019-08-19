7f92873fe4dc1e345014b6acab9311f001be70c3
Janis Strazdins <scan@inbox.lv>
2019-08-19 19:35:35 +0300

    added LOOPS exercise

---
+++ Loops/Exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.WriteLine("The first 10 natural numbers are: ");
+
+            for (var i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
